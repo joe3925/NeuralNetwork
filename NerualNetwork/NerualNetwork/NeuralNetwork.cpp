9cc91e3bdaeecc56d22b3ab098a06c2f864c93da
boden1203 <bodenboden212@gmail.com>
2023-10-23 22:33:58 -0700

    tried to add back-propagation with gradient descent

--- NerualNetwork/NerualNetwork/NeuralNetwork.cpp
+++ NerualNetwork/NerualNetwork/NeuralNetwork.cpp
@@ -7,10 +7,12 @@
 #include <cuchar>
 #include <string>
 #include <cstring>
+#include <thread>
+#include <windows.h>
 
 
 constexpr double Finaltarget = 1;
-constexpr double learningRate = 0.5;
+constexpr double learningRate = 0.1;
 
 constexpr int firstLayerNamount = 784;
 constexpr int  secondLayerNamount = 16;
@@ -32,18 +34,20 @@ std::vector<double> firstLayerW;
 std::vector<double> secondLayerW;
 std::vector<double> firstLayerWeighted;
 std::vector<double> secondLayerWeighted;
+std::vector<double> weightAdjustments;
+std::vector<int> label1 = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
 
 const std::string mnistImagesFile = "..\\TrainingData\\train-images-idx3-ubyte\\train-images.idx3-ubyte";
 const std::string mnistLabelsFile = "..\\TrainingData\\train-labels-idx1-ubyte\\train-labels.idx1-ubyte";
 
 double sigmoid (double m1);
-double cost(double target, double result);
-double fixWeight(double initalValue, double outputValue);
+double cost(std::vector <double> target, std::vector<double> result);
+double fixWeight(double initalValue, double outputValue, double currentLayer, double currentN);
 double xavierInitialization(int fan_in, int fan_out);
+double sumVector(std::vector<double> vector, int startAmount, int endAmount);
 void readMNISTData(std::vector<std::vector<double>>& images, std::vector<int>& labels);
-double sumVector(std::vector<double> vector,int startAmount, int endAmount);
-void TestNetwork(std::vector<double> image, int label, double& costReturn, int& result);
-
+void TestNetwork(std::vector<double> image, std::vector<int> label, double& costReturn, int& result);
 
 int main()
 {
@@ -55,8 +59,31 @@ int main()
     }
     double cost;
     int result;
+    double total = 0;
     readMNISTData(images, labels);
-    TestNetwork(images[0], labels[0], cost, result);
+    int i = 0;
+    while (i < images.size()) {
+        label1[labels[i]] = labels[i];
+        TestNetwork(images[i], label1, cost, result);
+        total = total + cost;
+        int j = 0;
+        int t = 0;
+        while (t < thirdLayerNamount) {
+            while (j < secondLayerW.size()) {
+                weightAdjustments.push_back(fixWeight(secondLayerW[j], thirdLayerN[t], 0.0, t));
+                j++;
+            }
+            t++;
+        }
+        i++;
+        firstLayerWeighted.clear();
+        secondLayerWeighted.clear();
+        secondLayerN.clear();
+        thirdLayerN.clear();
+    }
+    total = total / images.size();
+    std::cout << total << "\n";
+
     std::cout << cost << "\n";
     std::cout << result << "\n";
     return 0;
@@ -64,13 +91,21 @@ int main()
 double sigmoid (double m1) {
     return 1 / (1 + exp(-m1));
 }
-double cost(double target, double result)
+double cost(std::vector <int> target, std::vector<double> result)
 {
-    return ((target - result) * (target - result));
+    int i = 0;
+    double total = 0;
+    while (i < result.size()) {
+        total = total + (result[i] - target[i]) * (result[i] - target[i]);
+        i++;
+    }
+    
+    return total;
 }
-double fixWeight(double initalValue, double outputValue)
+//gradient descent is not fun
+double fixWeight(double initalValue, double outputValue, double currentLayer, double currentN)
 {
-    return initalValue + (learningRate* 2 *(Finaltarget- outputValue));
+    return initalValue - (learningRate * (thirdLayerN[currentN] * (2 * (thirdLayerN[currentN] * initalValue)) - 1));
 }
 double xavierInitialization(int fan_in, int fan_out) {
     // Use Xavier initialization (Glorot initialization)
@@ -111,18 +146,17 @@ double findLargest(std::vector<double> vec) {
     return position;
 }
 
-void TestNetwork(std::vector<double> image, int label, double &costReturn, int &result) {
+void TestNetwork(std::vector<double> image, std::vector<int> label, double &costReturn, int &result) {
     firstLayerN = image;
 
-
+    //TODO: fix this shit completly messed up the math     new N = sum(old N x W)/total N of previous layer 
 
     for (int i = 0; i < secondLayerNamount; i++) {
-        int j = 0;
-
+        //because of how I set everything up this multiplacation is needed (used list instead of a matrix)
         firstLayerWeighted.push_back(sumVector(firstLayerW, firstLayerNamount * i, firstLayerNamount * (i + 1)));
 
 
-        secondLayerN.push_back(sigmoid(firstLayerWeighted[i] / secondLayerNamount));
+        secondLayerN.push_back(sigmoid(firstLayerN[i] * (firstLayerWeighted[i] / secondLayerNamount)));
     }
 
 
@@ -130,14 +164,16 @@ void TestNetwork(std::vector<double> image, int label, double &costReturn, int &
 
     for (int i = 0; i < thirdLayerNamount; i++) {
         int j = 0;
-
+        //because of how I set everything up this multiplacation is needed (used list instead of a matrix)
         secondLayerWeighted.push_back(sumVector(secondLayerW, secondLayerNamount * i, secondLayerNamount * (i + 1)));
 
 
-        thirdLayerN.push_back(sigmoid(secondLayerWeighted[i] / thirdLayerNamount));
+        thirdLayerN.push_back(sigmoid( secondLayerN[i] * (secondLayerWeighted[i] / thirdLayerNamount)));
     }
+
     result = findLargest(thirdLayerN);
-    costReturn = cost(label, result);
+    costReturn = cost(label, thirdLayerN);
+
 }
 
 
