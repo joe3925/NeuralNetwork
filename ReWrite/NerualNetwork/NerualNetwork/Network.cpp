5fe7081758c0cc314d6b503011593bd1ac0014d6
boden1203 <bodenboden212@gmail.com>
2023-11-01 12:41:52 -0700

    doing a rewrite for it to be more organized

---
+++ ReWrite/NerualNetwork/NerualNetwork/Network.cpp
@@ -0,0 +1,104 @@
+﻿#include "Network.h"
+#include <iostream>
+#include <fstream>
+
+Network network;
+std::vector<std::vector<double>> images;
+std::vector<int> labels;
+const std::string mnistImagesFile = "C:\\Users\\Boden\\Documents\\NeuralNetwork\\ReWrite\\NerualNetwork\\TrainingData\\train-images-idx3-ubyte\\train-images.idx3-ubyte";
+const std::string mnistLabelsFile = "C:\\Users\\Boden\\Documents\\NeuralNetwork\\ReWrite\\NerualNetwork\\TrainingData\\train-labels-idx1-ubyte\\train-labels.idx1-ubyte";
+void readMNISTData(std::vector<std::vector<double>>&images1, std::vector<int>&labels1);
+int main()
+{
+    readMNISTData(images, labels);
+    if (network.intNetworkObject(images,labels, {784,16,10},network ))
+    {
+        std::cerr<<"unknown error with network initialization";
+        return 1;
+    }
+    if (network.xavierIntWeights(network))
+    {
+        std::cerr<<"Layers, nodesPerLayer or both were initialized wrong";
+        return 1;
+    }
+    if (network.intBias(network))
+    {
+        std::cerr<<"Layers, nodesPerLayer or both were initialized wrong";
+        return 1;
+    }
+    
+    images.clear();
+    labels.clear();
+    return 0;
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const int numImages = 60000;
+const int imageSize = 28 * 28;
+
+// Read MNIST images and labels
+void readMNISTData(std::vector<std::vector<double>>& images1, std::vector<int>& labels1) {
+    // Open the binary image file
+    std::ifstream imageStream(mnistImagesFile, std::ios::binary);
+    if (!imageStream) {
+        std::cerr << "Failed to open image file." << std::endl;
+        return;
+    }
+
+    // Open the binary label file
+    std::ifstream labelStream(mnistLabelsFile, std::ios::binary);
+    if (!labelStream) {
+        std::cerr << "Failed to open label file." << std::endl;
+        return;
+    }
+
+    char buffer[16];
+    imageStream.read(buffer, 16);
+
+    // Read the images
+    for (int i = 0; i < numImages; i++) {
+        std::vector<double> image(imageSize, 0.0);
+        for (int j = 0; j < imageSize; j++) {
+            unsigned char pixel;
+            imageStream.read(reinterpret_cast<char*>(&pixel), 1);
+            image[j] = static_cast<double>(pixel) / 255.0;  // Normalize pixel values to [0, 1]
+        }
+        images1.push_back(image);
+    }
+
+    // Read the MNIST label file header
+    char buffer1[8];
+    labelStream.read(buffer1, 8);
+
+    // Read the labels
+    for (int i = 0; i < numImages; i++) {
+        unsigned char label;
+        labelStream.read(reinterpret_cast<char*>(&label), 1);
+        labels1.push_back(static_cast<int>(label));
+    }
+
+    // Close the streams when done
+    imageStream.close();
+    labelStream.close();
+}
---
+++ ReWrite/NerualNetwork/NerualNetwork/Network.h
@@ -0,0 +1,97 @@
+﻿#pragma once
+#include <vector>
+#include <random>
+#include <chrono>
+#include <thread>
+
+using namespace std::chrono;
+double xavierInitialization(int fan_in, int fan_out);
+class Network
+{
+public:
+    
+    std::vector<std::vector<double>> images;
+    std::vector<int> labels;
+    std::vector<int> nodesPerLayer;
+    std::vector<std::vector<double>> bias;
+    std::vector<std::vector<double>> layersValues;
+    size_t layers;
+    std::vector<std::vector<std::vector<double>>> weights;
+
+    //recomended to just call intNetwork instead of setting values on your own
+    int intNetworkObject(std::vector<std::vector<double>> images1, std::vector<int> labels1, std::vector<int> nodesPerLayer1, Network& network)
+    {
+        std::vector<double> temp;
+        network.nodesPerLayer = nodesPerLayer1;
+        network.layers = nodesPerLayer1.size();
+        network.images = images1;
+        network.labels = labels1;
+        for (int i = 0; i < network.layers; i++)
+        {
+            layersValues.push_back(temp);
+            for (int j = 0; j < nodesPerLayer[i]; j++)
+            {
+                layersValues[i].push_back(0);
+            }
+        }
+        return 0;
+    }
+    
+    int xavierIntWeights(Network& network)
+    {
+        //temp values to resize the vectors since non const cant be passed in for size for some reason
+        std::vector<std::vector<double>> temp;
+        std::vector<double> temp1;
+        //check if values are valid
+        if (network.layers == NULL || network.nodesPerLayer[0] == NULL || (network.layers != network.nodesPerLayer.size()))
+        {
+            return 1;
+        }
+        for (int i = 0; i < network.layers - 1; i++)
+        {
+            network.weights.push_back(temp);
+            for(int j = 0; j < network.nodesPerLayer[i + 1]; j++)
+            {
+                network.weights[i].push_back(temp1);
+                for(int z = 0; z < network.nodesPerLayer[i]; z++)
+                {
+                    network.weights[i][j].push_back(xavierInitialization(nodesPerLayer[i], nodesPerLayer[i + 1]));
+                }
+            }
+        }
+        return 0;
+    }
+    int intBias(Network& network)
+    {
+       std::vector<double> temp;
+        if (network.layers == NULL || network.nodesPerLayer[0] == NULL || (network.layers != network.nodesPerLayer.size()))
+        {
+            return 1;
+        }
+        for (int i = 0; i < network.layers - 1; i++ )
+        {
+            network.bias.push_back(temp);
+            for (int j = 0; j < nodesPerLayer[i + 1]; j++)
+            {
+                network.bias[i].push_back(0.0);
+            }
+        }
+        return 0;
+    }
+};
+double xavierInitialization(int fan_in, int fan_out) {
+    // Use Xavier initialization (Glorot initialization)
+    auto start = std::chrono::high_resolution_clock::now();
+    double variance = 1.0 / (fan_in + fan_out);
+    double stddev = sqrt(variance);
+
+    // Generate random values with mean 0 and the calculated standard deviation
+    std::default_random_engine generator(time(0));
+    std::normal_distribution<double> distribution(0.0, stddev);
+    high_resolution_clock::time_point startTime = high_resolution_clock::now();
+    while ((std::chrono::high_resolution_clock::now() - start).count() < 0) {
+        int i = 1;
+        i = 0;
+    }
+    return distribution(generator);
+}
